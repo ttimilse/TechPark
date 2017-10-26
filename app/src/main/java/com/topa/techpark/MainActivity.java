93569e5147ecbe237460fdf4eff17c1b4c94dd3e
Kapil Maharjan <kapilmhr016@gmail.com>
2017-10-26 19:48:42 +0545

    park button clickable

--- app/src/main/java/com/topa/techpark/MainActivity.java
+++ app/src/main/java/com/topa/techpark/MainActivity.java
@@ -1,15 +1,25 @@
 package com.topa.techpark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button findPark;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        findPark = (Button) findViewById(R.id.button_find_park);
+        findPark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,find_parking.class);
+                startActivity(i);
+            }
+        });
     }
 
 
